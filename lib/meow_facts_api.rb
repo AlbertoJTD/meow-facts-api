be8e2a39ab540db831c62c3454d41861b69eb7e9
AlbertoJTD <albertojtd@outlook.com>
2025-04-20 14:45:32 -0600

    Add API functionality to fetch cat facts and update tests for the new method

--- lib/meow_facts_api.rb
+++ lib/meow_facts_api.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
-require_relative "meow_facts_api/version"
+require_relative 'meow_facts_api/version'
+require 'net/http'
+require 'json'
 
 module MeowFactsApi
-  class Error < StandardError; end
-  # Your code goes here...
+  API_URL = URI('https://meowfacts.herokuapp.com/')
+
+  class << self
+    def fact
+      response = Net::HTTP.get_response(API_URL)
+      JSON.parse(response.body)
+    rescue StandardError => e
+      {
+        'error' => "Error fetching cat fact: #{e.message}"
+      }.to_json
+    end
+  end
 end
--- spec/meow_facts_api_spec.rb
+++ spec/meow_facts_api_spec.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
 RSpec.describe MeowFactsApi do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MeowFactsApi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context '.fact' do
+    let(:successful_response) do
+      {
+        'data' => ['Cats have been domesticated for around 4,000 years.']
+      }.to_json
+    end
+
+    before do
+      allow(Net::HTTP).to receive(:get_response).and_return(
+        instance_double(Net::HTTPResponse, body: successful_response)
+      )
+    end
+
+    it 'returns a cat fact when the API call is successful' do
+      result = described_class.fact
+      expect(result).to be_a(Hash)
+      expect(result['data']).to be_an(Array)
+      expect(result['data'].first).to be_a(String)
+      expect(result.keys).to include('data')
+    end
   end
 end
